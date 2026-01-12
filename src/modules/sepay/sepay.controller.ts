5281ce4736f996f9708ecb0fb4b77a538fc83b44
Dung <dungtdhe186461@fpt.edu.vn>
2026-01-12 23:09:29 +0700

    hook

--- src/modules/sepay/sepay.controller.ts
+++ src/modules/sepay/sepay.controller.ts
@@ -6,12 +6,54 @@ import { TransactionStatus, TransactionType } from '@prisma/client';
 export class SepayController {
   constructor(private prisma: PrismaService) {}
 
+  // @Post('webhook')
+  // async handleWebhook(@Body() body: any) {
+  //   if (!body) return { ok: true };
+
+  //   const { amount, content, status } = body;
+  //   if (!content || !amount || status !== 'SUCCESS') return { ok: true };
+
+  //   const tx = await this.prisma.walletTransaction.findFirst({
+  //     where: {
+  //       sepayOrderId: content,
+  //       type: TransactionType.RENTAL_PENDING,
+  //     },
+  //   });
+
+  //   if (!tx) return { ok: true };
+
+  //   // 🔒 ATOMIC UPDATE
+  //   const updated = await this.prisma.walletTransaction.updateMany({
+  //     where: {
+  //       id: tx.id,
+  //       status: TransactionStatus.PENDING,
+  //     },
+  //     data: {
+  //       status: TransactionStatus.SUCCESS,
+  //       confirmedAt: new Date(),
+  //     },
+  //   });
+
+  //   if (updated.count === 0) return { ok: true };
+
+  //   await this.prisma.wallet.update({
+  //     where: { id: tx.walletId },
+  //     data: {
+  //       pendingBalance: { increment: tx.amount * 0.9 },
+  //     },
+  //   });
+
+  //   return { ok: true };
+  // }
   @Post('webhook')
   async handleWebhook(@Body() body: any) {
     if (!body) return { ok: true };
 
-    const { amount, content, status } = body;
-    if (!content || !amount || status !== 'SUCCESS') return { ok: true };
+    const { content, transferType, transferAmount } = body;
+
+    // Kiểm tra content tồn tại và là giao dịch nhận tiền
+    if (!content || transferType !== 'in' || !transferAmount)
+      return { ok: true };
 
     const tx = await this.prisma.walletTransaction.findFirst({
       where: {
